1df60bf224631d403f0fd0f271e4aa33db2121af
shogohamda <>
2022-04-04 19:35:37 +0900

    secret追加

--- terraform/secret.tf
+++ terraform/secret.tf
@@ -12,3 +12,18 @@ resource "google_secret_manager_secret" "secret-db" {
     }
   }
 }
+
+resource "google_secret_manager_secret" "superuser-password" {
+  secret_id = "superuser_password"
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+      replicas {
+        location = "asia-northeast1"
+      }
+    }
+  }
+}
