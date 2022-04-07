351d8b192261b88204e3d89b85263785afa3e753
shogohamda <>
2022-04-07 15:20:43 +0900

    公開用バケットの作成

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,21 @@
-resource "google_storage_bucket" "static-bucket" {
+# For django-storages
+resource "google_storage_bucket" "media-bucket" {
   name          = "${var.project_id}-media-bucket"
   location      = "ASIA"
   force_destroy = true
+}
+
+# For static files
+resource "google_storage_bucket" "static-bucket" {
+  name          = "${var.project_id}-static-bucket"
+  location      = "ASIA"
+  force_destroy = true
+}
+
+resource "google_storage_bucket_iam_binding" "static-bucket" {
+  bucket = google_storage_bucket.static-bucket.name
+    role = "roles/storage.legacyObjectReader"
+    members = [
+      "allUsers",
+    ]
 }
